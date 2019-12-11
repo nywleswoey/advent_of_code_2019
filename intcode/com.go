106310a832984daa5dbeb5d559f59d23e8b4217e
Selwyn Yeow <selwyn_yeow@nus.edu.sg>
2019-12-11 22:32:26 +0800

    Refactor IntCode computer

--- intcode/com.go
+++ intcode/com.go
@@ -1,11 +1,7 @@
 package intcode
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 const (
@@ -24,152 +20,186 @@ func cloneArray(a []int) []int {
 	return append(a[:0:0], a...)
 }
 
-// Program represents an Intcode computer program
-type Program struct {
-	prog   []int
-	reader *bufio.Reader
-}
-
-// NewProgram creates a new Intcode computer program
-func NewProgram(prog []int) Program {
-	return Program{prog: cloneArray(prog), reader: bufio.NewReader(os.Stdin)}
-}
-
 func extractMode(modes int) (int, int) {
 	// returns the right most mode and the remaining unused modes
 	return modes % 10, modes / 10
 }
 
-func getValue(prog []int, param, mode int) int {
+// Program represents an Intcode computer program
+type Program struct {
+	Instructions []int
+	instrPtr     int
+	IsDone       bool
+}
+
+// NewProgram creates a new Intcode computer program
+func NewProgram(prog []int) Program {
+	return Program{Instructions: cloneArray(prog), instrPtr: 0}
+}
+
+func (p Program) getValue(param, mode int) int {
 	if mode == 1 {
 		return param
 	}
-	return prog[param]
+	return p.Instructions[param]
 }
 
-func executeAdd(prog []int, param1, param2, param3, modes int) int {
+func (p *Program) executeAdd(modes int) {
+	param1 := p.Instructions[p.instrPtr+1]
+	param2 := p.Instructions[p.instrPtr+2]
+	param3 := p.Instructions[p.instrPtr+3]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
+	value1 := p.getValue(param1, mode)
 	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
+	value2 := p.getValue(param2, mode)
 	mode, modes = extractMode(modes)
 	if mode != 0 {
 		// do some error handling
 	}
-	prog[param3] = value1 + value2
-	return 4
+	p.Instructions[param3] = value1 + value2
+	p.instrPtr += 4
 }
 
-func executeMult(prog []int, param1, param2, param3, modes int) int {
+func (p *Program) executeMult(modes int) {
+	param1 := p.Instructions[p.instrPtr+1]
+	param2 := p.Instructions[p.instrPtr+2]
+	param3 := p.Instructions[p.instrPtr+3]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
+	value1 := p.getValue(param1, mode)
 	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
+	value2 := p.getValue(param2, mode)
 	mode, modes = extractMode(modes)
 	if mode != 0 {
 		// do some error handling
 	}
-	prog[param3] = value1 * value2
-	return 4
+	p.Instructions[param3] = value1 * value2
+	p.instrPtr += 4
 }
 
-func executeSave(prog []int, param1, input int) int {
-	prog[param1] = input
-	return 2
+func (p *Program) executeSave(input int) {
+	param1 := p.Instructions[p.instrPtr+1]
+
+	p.Instructions[param1] = input
+	p.instrPtr += 2
 }
 
-func executeLoad(prog []int, param1, modes int) int {
+func (p *Program) executeLoad(modes int) int {
+	param1 := p.Instructions[p.instrPtr+1]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	fmt.Printf("output: %v\n", value1)
-	return 2
+	value := p.getValue(param1, mode)
+	p.instrPtr += 2
+	return value
 }
 
-func executeJump(onTrue bool, prog []int, param1, param2, modes, currPtr int) int {
+func (p *Program) executeJump(onTrue bool, modes int) {
+	param1 := p.Instructions[p.instrPtr+1]
+	param2 := p.Instructions[p.instrPtr+2]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
+	value1 := p.getValue(param1, mode)
 	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
+	value2 := p.getValue(param2, mode)
 	if (onTrue && value1 != 0) || (!onTrue && value1 == 0) {
-		return value2
+		p.instrPtr = value2
+	} else {
+		p.instrPtr += 3
 	}
-	return currPtr + 3
 }
 
-func executeLessThan(prog []int, param1, param2, param3, modes int) int {
+func (p *Program) executeLessThan(modes int) {
+	param1 := p.Instructions[p.instrPtr+1]
+	param2 := p.Instructions[p.instrPtr+2]
+	param3 := p.Instructions[p.instrPtr+3]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
+	value1 := p.getValue(param1, mode)
 	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
+	value2 := p.getValue(param2, mode)
 	mode, modes = extractMode(modes)
 	if mode != 0 {
 		// do some error handling
 	}
 
 	if value1 < value2 {
-		prog[param3] = 1
+		p.Instructions[param3] = 1
 	} else {
-		prog[param3] = 0
+		p.Instructions[param3] = 0
 	}
-	return 4
+	p.instrPtr += 4
 }
 
-func executeEquals(prog []int, param1, param2, param3, modes int) int {
+func (p *Program) executeEquals(modes int) {
+	param1 := p.Instructions[p.instrPtr+1]
+	param2 := p.Instructions[p.instrPtr+2]
+	param3 := p.Instructions[p.instrPtr+3]
+
 	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
+	value1 := p.getValue(param1, mode)
 	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
+	value2 := p.getValue(param2, mode)
 	mode, modes = extractMode(modes)
 	if mode != 0 {
 		// do some error handling
 	}
 
 	if value1 == value2 {
-		prog[param3] = 1
+		p.Instructions[param3] = 1
 	} else {
-		prog[param3] = 0
+		p.Instructions[param3] = 0
 	}
-	return 4
+	p.instrPtr += 4
 }
 
 // Process will run the Intcode program using the inputs
-func (p Program) Process() {
-	var instrPtr int
-	prog := p.prog
-	reader := p.reader
+func (p *Program) Process(input <-chan int) <-chan int {
+	prog := p.Instructions
+	output := make(chan int)
 
-Done:
-	for instrPtr < len(prog) {
-		opCode := prog[instrPtr] % 100
-		modes := prog[instrPtr] / 100
+	go func() {
+	Done:
+		for p.instrPtr < len(prog) {
+			opCode := prog[p.instrPtr] % 100
+			modes := prog[p.instrPtr] / 100
 
-		switch opCode {
-		case add:
-			instrPtr += executeAdd(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case mult:
-			instrPtr += executeMult(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case save:
-			text, _ := reader.ReadString('\n')
-			text = strings.Replace(text, "\n", "", -1)
-			input, _ := strconv.Atoi(text)
-			instrPtr += executeSave(prog, prog[instrPtr+1], input)
-		case load:
-			// for this puzzle we'll just print to console
-			instrPtr += executeLoad(prog, prog[instrPtr+1], modes)
-		case jumpOnTrue:
-			instrPtr = executeJump(true, prog, prog[instrPtr+1], prog[instrPtr+2], modes, instrPtr)
-		case jumpOnFalse:
-			instrPtr = executeJump(false, prog, prog[instrPtr+1], prog[instrPtr+2], modes, instrPtr)
-		case lessThan:
-			instrPtr += executeLessThan(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case equals:
-			instrPtr += executeEquals(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case end:
-			instrPtr++
-			break Done
-		default:
-			fmt.Printf("encountered an invalid op code: %v at address %v\n", prog[instrPtr], instrPtr)
-			break Done
+			switch opCode {
+			case add:
+				p.executeAdd(modes)
+			case mult:
+				p.executeMult(modes)
+			case save:
+				isWaiting := true
+				for isWaiting {
+					select {
+					case result := <-input:
+						isWaiting = false
+						p.executeSave(result)
+					}
+				}
+			case load:
+				output <- p.executeLoad(modes)
+			case jumpOnTrue:
+				p.executeJump(true, modes)
+			case jumpOnFalse:
+				p.executeJump(false, modes)
+			case lessThan:
+				p.executeLessThan(modes)
+			case equals:
+				p.executeEquals(modes)
+			case end:
+				p.instrPtr++
+				break Done
+			default:
+				fmt.Printf("encountered an invalid op code: %v at address %v\n", p.Instructions[p.instrPtr], p.instrPtr)
+				break Done
+			}
 		}
-	}
+
+		p.IsDone = true
+		close(output)
+	}()
+
+	return output
 }
--- src/day5/main.go
+++ src/day5/main.go
@@ -2,155 +2,34 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/nywleswoey/advent_of_code_2019/intcode"
 )
 
-const (
-	add         int = 1
-	mult        int = 2
-	save        int = 3
-	load        int = 4
-	jumpOnTrue  int = 5
-	jumpOnFalse int = 6
-	lessThan    int = 7
-	equals      int = 8
-	end         int = 99
-)
-
-func cloneArray(a []int) []int {
-	return append(a[:0:0], a...)
-}
-
-func extractMode(modes int) (int, int) {
-	// returns the right most mode and the remaining unused modes
-	return modes % 10, modes / 10
-}
-
-func getValue(prog []int, param, mode int) int {
-	if mode == 1 {
-		return param
-	}
-	return prog[param]
-}
-
-func executeAdd(prog []int, param1, param2, param3, modes int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
-	mode, modes = extractMode(modes)
-	if mode != 0 {
-		// do some error handling
-	}
-	prog[param3] = value1 + value2
-	return 4
-}
-
-func executeMult(prog []int, param1, param2, param3, modes int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
-	mode, modes = extractMode(modes)
-	if mode != 0 {
-		// do some error handling
-	}
-	prog[param3] = value1 * value2
-	return 4
-}
-
-func executeSave(prog []int, param1, input int) int {
-	prog[param1] = input
-	return 2
-}
-
-func executeLoad(prog []int, param1, modes int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	fmt.Printf("output: %v\n", value1)
-	return 2
-}
-
-func executeJump(onTrue bool, prog []int, param1, param2, modes, currPtr int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
-	if (onTrue && value1 != 0) || (!onTrue && value1 == 0) {
-		return value2
-	}
-	return currPtr + 3
-}
-
-func executeLessThan(prog []int, param1, param2, param3, modes int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
-	mode, modes = extractMode(modes)
-	if mode != 0 {
-		// do some error handling
-	}
-
-	if value1 < value2 {
-		prog[param3] = 1
-	} else {
-		prog[param3] = 0
-	}
-	return 4
-}
-
-func executeEquals(prog []int, param1, param2, param3, modes int) int {
-	mode, modes := extractMode(modes)
-	value1 := getValue(prog, param1, mode)
-	mode, modes = extractMode(modes)
-	value2 := getValue(prog, param2, mode)
-	mode, modes = extractMode(modes)
-	if mode != 0 {
-		// do some error handling
-	}
-
-	if value1 == value2 {
-		prog[param3] = 1
-	} else {
-		prog[param3] = 0
-	}
-	return 4
-}
-
-func process(prog []int, input int) {
-	var instrPtr int
+func startWithoutInput(data []int) []int {
+	program := intcode.NewProgram(data)
+	in := make(chan int)
+	out := program.Process(in)
 
 Done:
-	for instrPtr < len(prog) {
-		opCode := prog[instrPtr] % 100
-		modes := prog[instrPtr] / 100
-
-		switch opCode {
-		case add:
-			instrPtr += executeAdd(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case mult:
-			instrPtr += executeMult(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case save:
-			instrPtr += executeSave(prog, prog[instrPtr+1], input)
-		case load:
-			// for this puzzle we'll just print to console
-			instrPtr += executeLoad(prog, prog[instrPtr+1], modes)
-		case jumpOnTrue:
-			instrPtr = executeJump(true, prog, prog[instrPtr+1], prog[instrPtr+2], modes, instrPtr)
-		case jumpOnFalse:
-			instrPtr = executeJump(false, prog, prog[instrPtr+1], prog[instrPtr+2], modes, instrPtr)
-		case lessThan:
-			instrPtr += executeLessThan(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case equals:
-			instrPtr += executeEquals(prog, prog[instrPtr+1], prog[instrPtr+2], prog[instrPtr+3], modes)
-		case end:
-			instrPtr++
-			break Done
-		default:
-			fmt.Printf("encountered an invalid op code: %v at address %v\n", prog[instrPtr], instrPtr)
+	for {
+		select {
+		case <-out:
 			break Done
 		}
 	}
+
+	return program.Instructions
+}
+
+func startWithInitialInput(data []int, value int) <-chan int {
+	program := intcode.NewProgram(data)
+	in := make(chan int)
+	out := program.Process(in)
+
+	in <- value
+
+	return out
 }
 
 func main() {
@@ -161,15 +40,17 @@ func main() {
 	}
 
 	for _, v := range testData {
-		fmt.Printf("Received value: %v, ", v)
-		process(v, 1)
-		fmt.Printf("Processed value: %v\n\n", v)
+		fmt.Printf("Received value: %v, Processed value: %v\n\n", v, startWithoutInput(v))
 	}
 
 	puzzleInput := []int{3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 72, 36, 225, 1101, 87, 26, 225, 2, 144, 13, 224, 101, -1872, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1102, 66, 61, 225, 1102, 25, 49, 224, 101, -1225, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 5, 224, 1, 223, 224, 223, 1101, 35, 77, 224, 101, -112, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1002, 195, 30, 224, 1001, 224, -2550, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 1, 224, 1, 224, 223, 223, 1102, 30, 44, 225, 1102, 24, 21, 225, 1, 170, 117, 224, 101, -46, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 5, 224, 224, 1, 224, 223, 223, 1102, 63, 26, 225, 102, 74, 114, 224, 1001, 224, -3256, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 224, 223, 223, 1101, 58, 22, 225, 101, 13, 17, 224, 101, -100, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 6, 224, 224, 1, 224, 223, 223, 1101, 85, 18, 225, 1001, 44, 7, 224, 101, -68, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 5, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 329, 101, 1, 223, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 344, 1001, 223, 1, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 359, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 374, 101, 1, 223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 389, 101, 1, 223, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 404, 101, 1, 223, 223, 1008, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 419, 1001, 223, 1, 223, 107, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 434, 101, 1, 223, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 449, 101, 1, 223, 223, 1108, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 464, 101, 1, 223, 223, 1007, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 479, 101, 1, 223, 223, 1008, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 494, 101, 1, 223, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 509, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 524, 101, 1, 223, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 539, 1001, 223, 1, 223, 108, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 554, 101, 1, 223, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 569, 101, 1, 223, 223, 8, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 584, 101, 1, 223, 223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 599, 1001, 223, 1, 223, 107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 614, 1001, 223, 1, 223, 1108, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 629, 101, 1, 223, 223, 7, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 644, 1001, 223, 1, 223, 108, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 659, 101, 1, 223, 223, 1007, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 674, 101, 1, 223, 223, 4, 223, 99, 226}
 
 	// puzzle 1 answer
-	process(cloneArray(puzzleInput), 1)
+	var output int
+	for output = range startWithInitialInput(puzzleInput, 1) {
+		fmt.Printf("Output: %v\n", output)
+	}
+	fmt.Printf("Puzzle 1 answer: %v\n", output)
 
 	testData = [][]int{
 		[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
@@ -193,10 +74,10 @@ func main() {
 
 	for i, data := range testData {
 		for _, input := range testInput[i] {
-			process(cloneArray(data), input)
+			fmt.Printf("for %v with input %v, output: %v\n", data, input, <-startWithInitialInput(data, input))
 		}
 	}
 
 	// puzzle 2 answer
-	process(cloneArray(puzzleInput), 5)
+	fmt.Printf("Puzzle 2 answer: %v\n", <-startWithInitialInput(puzzleInput, 5))
 }
